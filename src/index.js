f7a347a7f4d556279ad2a8f80e860657e45da404
yantomchik <55480251+YanTomchik@users.noreply.github.com>
2020-10-03 15:52:42 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 exports.min = function min (array) {
+
+  if (array === undefined || array[0] === undefined)
   return 0;
+  else 
+  return Math.min(...array)
+
 }
 
 exports.max = function max (array) {
+  if (array === undefined || array[0] === undefined)
   return 0;
+  else 
+  return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
+  if (array === undefined || array[0] === undefined)
   return 0;
+  else{
+    let avg = array.reduce((sum, current) => sum + current, 0) / array.length ;
+    return avg;
+  }
+  
 }
